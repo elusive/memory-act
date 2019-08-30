fa537a598d376f72ec8a0acf0100c507c3285a83
John Gilliland <john.gilliland@rndgroup.com>
2019-08-29 21:13:52 -0400

    Added a greeting modal dialog with button.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React from 'react';
 /* import icon images */
 import './App.css';
 import Header from './components/Header';
+import Modal from './components/Modal';
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Modal />
     </div>
   );
 }
